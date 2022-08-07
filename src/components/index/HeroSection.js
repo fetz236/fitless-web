35f1f51e4ecfa9349838659e4fb90e52781d165c
fetz236 <58368484+fetz236@users.noreply.github.com>
2022-08-07 20:20:53 +0300

    Fixed mobile view

--- src/components/index/HeroSection.js
+++ src/components/index/HeroSection.js
@@ -6,7 +6,6 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
       <h1>Fitless</h1>
       <p>Revolutionising Fitness</p>
       <div className='hero-btns'>
